a70e285b87ebf732b130657b236c78b5aeb1d3e9
jonyShwartz <105234783+jonyShwartz@users.noreply.github.com>
2023-05-07 01:26:01 +0300

    add file

---
+++ pizza/index.js
@@ -0,0 +1,8 @@
+document.getElementById("send").addEventListener("click", click);
+
+function click{
+    let type = document.getElementById("select").value;
+    let size = document.getElementById("select1").value;
+alert(type,size )
+}
+
